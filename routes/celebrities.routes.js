37fb99611915260d435e42c419c7036ec070fcf5
David Sanchez Valera <chirlo14@gmail.com>
2023-11-07 18:11:22 +0100

    bonus solved

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -34,4 +34,38 @@ router.get("/", async (req, res, next) => {
   }
 });
 
+//POST "/celebrities/:id/delete" => buscar la pelicula en DB por id y borrarla
+router.post("/:id/delete", async (req, res, next) => {
+  try {
+    await Celebrity.findByIdAndDelete(req.params.id);
+    res.redirect("/celebrities");
+  } catch (err) {
+    next(err);
+  }
+});
+
+//GET "/celebrities/:id/edit" => mostrar el formulario para editar la pelicula
+router.get("/:id/edit", async (req, res, next) => {
+  try {
+    const findCelebrity = await Celebrity.findById(req.params.id);
+    res.render("celebrities/edit-celebrity.hbs", { findCelebrity });
+  } catch (err) {
+    next(err);
+  }
+});
+
+//POST "/celebrities/:id/edit" => mandar los cambios a la DB
+router.post("/:id/edit", async (req, res, next) => {
+  try {
+    await Celebrity.findByIdAndUpdate(req.params.id, {
+      name: req.body.name,
+      occupation: req.body.occupation,
+      catchPhrase: req.body.catchPhrase,
+    });
+    res.redirect(`/celebrities`);
+  } catch (err) {
+    next(err);
+  }
+});
+
 module.exports = router;
